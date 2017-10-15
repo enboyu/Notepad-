421f3fee51e59864aaa7cef7fc8173efc9992b3b
enboyu <yu121yufreeze@gmail.com>
2017-10-15 14:40:00 -0400

    final project- notepad + first step: vbox

---
+++ src/edu/bsu/cs222/notepad1/Controller.java
@@ -0,0 +1,9 @@
+package edu.bsu.cs222.notepad1;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void Newfilesbutton(ActionEvent actionEvent) {
+        System.out.print(" ");
+    }
+}
